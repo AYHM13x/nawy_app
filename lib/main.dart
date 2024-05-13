2d54da8d21d32cb3ef19583892e9e9533fa57f05
AYHM13x <ayhmalakel1995@gmail.com>
2024-05-13 15:10:54 +0300

    add map

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:table_calendar/table_calendar.dart';
-
-// import 'package:custom_navigation_bar/custom_navigation_bar.dart';
-// import 'package:doptica_app/constans.dart';
-// import 'package:doptica_app/core/utils/app_style.dart';
-// import 'package:doptica_app/core/widgets/custom_app_bar.dart';
-// import 'package:doptica_app/featurs/edit_profile_feature.dart/edit_profile_view.dart';
-// import 'package:doptica_app/featurs/home_feature/home_view.dart';
-// import 'package:doptica_app/featurs/home_feature/widgets/custom_grid_view.dart';
-// import 'package:doptica_app/featurs/home_feature/widgets/custom_navigator_bar.dart';
-// import 'package:doptica_app/featurs/profile_feature/profile_view.dart';
-// import 'package:doptica_app/featurs/tasks_featurs/tasks_view.dart';
-// import 'package:flutter/cupertino.dart';
-// // import 'package:flutter/material.dart';
-// import 'package:flutter/painting.dart';
-// import 'package:flutter/rendering.dart';
-// import 'package:flutter/widgets.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const NawyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class NawyApp extends StatelessWidget {
+  const NawyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: HomeViewBody(),
       ),
@@ -35,58 +21,26 @@ class MyApp extends StatelessWidget {
 }
 
 class HomeViewBody extends StatelessWidget {
-  const HomeViewBody({
-    super.key,
-  });
+  const HomeViewBody({super.key});
+
   @override
   Widget build(BuildContext context) {
-    final DateTime tody = DateTime.now();
-
-    return SafeArea(
-      child: Column(
-        children: [
-          const SizedBox(
-            height: 0,
-          ),
-          // const CustomAppar(),
-          // const CoustomGridView(),
-          Container(
-            decoration: BoxDecoration(
-                color: Colors.blue, borderRadius: BorderRadius.circular(20)),
-            height: 220,
-            width: 320,
-            child: Column(
-              children: [
-                const Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        "Calendar",
-                      ),
-                      Icon(Icons.calendar_month, color: Colors.black)
-                    ],
-                  ),
-                ),
-                SizedBox(
-                  width: 221,
-                  height: 169,
-                  child: TableCalendar(
-                    focusedDay: tody,
-                    firstDay: DateTime.utc(2010),
-                    lastDay: DateTime.utc(2040),
-                    availableGestures: AvailableGestures.all,
-                  ),
-                ),
-              ],
-            ),
-          ),
-          const SizedBox(
-            height: 70,
-          )
-        ],
+    return FlutterMap(
+      options: const MapOptions(
+        initialCenter: LatLng(33.30, 36.17),
+        initialZoom: 8,
       ),
+      children: [
+        TileLayer(
+          urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+          // userAgentPackageName: 'com.example.app',
+          subdomains: const [
+            'a',
+            'b',
+            'c',
+          ],
+        ),
+      ],
     );
   }
 }

6211ff76001a1006c00c487b74652a6c87dd87ff
AYHM13x <ayhmalakel1995@gmail.com>
2024-05-14 11:37:28 +0300

    test map

--- lib/main.dart
+++ lib/main.dart
@@ -27,18 +27,20 @@ class HomeViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return FlutterMap(
       options: const MapOptions(
-        initialCenter: LatLng(33.30, 36.17),
-        initialZoom: 8,
-      ),
+          //initialCenter: LatLng(33.30, 36.17),
+          //initialZoom: 8,
+          ),
       children: [
         TileLayer(
-          urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+          urlTemplate:
+              "https://api.mapbox.com/styles/v1/ayhm13x/clw62ut9602kt01qr46ac4hyk/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiYXlobTEzeCIsImEiOiJjbHc2MXkzNTcxaDhvMmttcTV0NzQ2M3h3In0.rja3psgBxQyRZs0JqCQwqg",
+          // urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
           // userAgentPackageName: 'com.example.app',
-          subdomains: const [
-            'a',
-            'b',
-            'c',
-          ],
+          additionalOptions: const {
+            "accessToken":
+                "pk.eyJ1IjoiYXlobTEzeCIsImEiOiJjbHc2MXkzNTcxaDhvMmttcTV0NzQ2M3h3In0.rja3psgBxQyRZs0JqCQwqg",
+            "id": "mapbox.mapbox-streets-v8",
+          },
         ),
       ],
     );

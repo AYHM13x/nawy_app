05bb0948a1a2331d6e33b0be4e0a787edb330a6c
AYHM13x <ayhmalakel1995@gmail.com>
2024-05-16 15:50:13 +0300

    work on localization

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_map/flutter_map.dart';
-import 'package:latlong2/latlong.dart';
-import 'package:nawy_app/core/utlis/assets/app_font_families.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:intl/intl.dart';
+import 'package:gap/gap.dart';
+import 'core/utlis/assets/app_font_families.dart';
+import 'generated/l10n.dart';
 
 void main() {
   runApp(const NawyApp());
@@ -13,55 +15,36 @@ class NawyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      locale: const Locale("ar"),
+      localizationsDelegates: const [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: S.delegate.supportedLocales,
       debugShowCheckedModeBanner: false,
+      onGenerateTitle: (BuildContext context) => S.of(context).title,
+      // title: S.of(context).title,
       theme: ThemeData(
         fontFamily: AppFontFamilies.cairoFont,
       ),
-      home: const Scaffold(
-        body: HomeViewBody(),
-      ),
-    );
-  }
-}
-
-class HomeViewBody extends StatefulWidget {
-  const HomeViewBody({super.key});
-
-  @override
-  State<HomeViewBody> createState() => _HomeViewBodyState();
-}
-
-class _HomeViewBodyState extends State<HomeViewBody> {
-  @override
-  Widget build(BuildContext context) {
-    return FlutterMap(
-      options: const MapOptions(
-        initialCenter: LatLng(33.30, 36.17),
-        initialZoom: 10,
-      ),
-      children: [
-        TileLayer(
-          urlTemplate:
-              "https://api.mapbox.com/styles/v1/ayhm13x/clw62ut9602kt01qr46ac4hyk/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiYXlobTEzeCIsImEiOiJjbHc2MXkzNTcxaDhvMmttcTV0NzQ2M3h3In0.rja3psgBxQyRZs0JqCQwqg",
-          // urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-          // userAgentPackageName: 'com.example.app',
-          additionalOptions: const {
-            'accessToken':
-                'pk.eyJ1IjoiYXlobTEzeCIsImEiOiJjbHc2MXkzNTcxaDhvMmttcTV0NzQ2M3h3In0.rja3psgBxQyRZs0JqCQwqg',
-            'id': 'mapbox.mapbox-streets-v8',
-          },
+      home: Scaffold(
+        appBar: AppBar(
+          title: Builder(
+            builder: (BuildContext context) => Text(S.of(context).title),
+          ),
         ),
-        const MarkerLayer(
-          markers: [
-            Marker(
-              point: LatLng(33.30, 36.17),
-              child: Icon(
-                Icons.pin_drop,
-              ),
+        // body : Text(S.of(context).title)
+        body: Column(
+          children: [
+            const Gap(50),
+            Builder(
+              builder: (BuildContext context) => Text(S.of(context).title),
             ),
           ],
         ),
-      ],
+      ),
     );
   }
 }

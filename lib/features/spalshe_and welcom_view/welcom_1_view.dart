24c89871b6fb95b216e89d6514d9135fd43becbd
mohamad darweshe <@gmail.com>
2024-05-19 16:04:51 +0300

    ubdate app

--- lib/features/spalshe_and welcom_view/welcom_1_view.dart
+++ lib/features/spalshe_and welcom_view/welcom_1_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:nawy_app/core/utlis/assets/app_images.dart';
 import 'package:nawy_app/features/spalshe_and%20welcom_view/spalshe_view.dart';
+import 'package:nawy_app/features/spalshe_and%20welcom_view/wodgets/coustom_app_bar.dart';
 
 class WelcomOneView extends StatelessWidget {
   const WelcomOneView({super.key});
@@ -11,36 +12,15 @@ class WelcomOneView extends StatelessWidget {
     return const SafeArea(
       child: Scaffold(
         backgroundColor: Colors.white,
-        body: Column(
-          children: [coustomAppar()],
+        body: Padding(
+          padding: EdgeInsets.all(30),
+          child: Column(
+            children: [
+              coustomAppar(),
+            ],
+          ),
         ),
       ),
-    );
-  }
-}
-
-class coustomAppar extends StatelessWidget {
-  const coustomAppar({super.key});
-
-  get child => null;
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        IconButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => SplaseView()),
-              );
-            },
-            icon: Icon(Icons.arrow_back)),
-        SvgPicture.asset(
-          AppImages.splashLogo2Image,
-        )
-      ],
     );
   }
 }

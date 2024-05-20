4a17f46398c1f927bee47ac2bcfec7a99003c43f
AYHM13x <ayhmalakel1995@gmail.com>
2024-05-20 06:20:15 +0300

    add some images

--- lib/core/utlis/assets/app_images.dart
+++ lib/core/utlis/assets/app_images.dart
@@ -4,9 +4,10 @@ abstract class AppImages {
   static const markerImage = "$_basePath/marker.svg";
   static const splashLogo2Image = "$_basePath/splash_logo2.svg";
   static const pana = "$_basePath/pana.svg";
-  static const homeIcon = "$_basePath/home.svg";
-  static const mapIcon = "$_basePath/map.svg";
+  static const homeIcon = "$_basePath/home_icon.svg";
+  static const mapIcon = "$_basePath/map_icon.svg";
   static const addIcon = "$_basePath/add_icon.svg";
   static const myOrdersIcon = "$_basePath/my_orders_icon.svg";
   static const chatsIcon = "$_basePath/chats_icon.svg";
+  static const listIcon = "$_basePath/list_icon.svg";
 }

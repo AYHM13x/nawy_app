03fc99f964625c4fa23ff31f6ceae2bf7d42d17b
mohamad darweshe <@gmail.com>
2024-05-21 18:19:37 +0300

    Finish custom completed

--- lib/features/_2_home/presentation/views/bodies/add_view/add_view_body.dart
+++ lib/features/_2_home/presentation/views/bodies/add_view/add_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:nawy_app/features/_0_spalsh/presentation/views/appbars/coustom_app_bar.dart';
+import 'package:nawy_app/features/_2_home/presentation/views/bodies/add_view/widgets/Custom_Completed.dart';
 import 'package:nawy_app/features/_2_home/presentation/views/bodies/add_view/widgets/Custom_app_bar.dart';
 
 class AddBodyView extends StatelessWidget {
@@ -8,7 +9,7 @@ class AddBodyView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: [CustomAppBarAddView()],
+      children: [CustomAppBarAddView(), CustomCompleted()],
     );
   }
 }

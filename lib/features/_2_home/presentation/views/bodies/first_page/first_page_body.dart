97d3d51cde050dbbac0e096293a30c533244b2c6
mohamad darweshe <@gmail.com>
2024-05-21 13:16:10 +0300

    finish custom searsh

--- lib/features/_2_home/presentation/views/bodies/first_page/first_page_body.dart
+++ lib/features/_2_home/presentation/views/bodies/first_page/first_page_body.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:nawy_app/core/utlis/assets/app_colors.dart';
 import 'package:nawy_app/features/_2_home/presentation/views/bodies/first_page/widgets/custom_head.dart';
+import 'package:nawy_app/features/_2_home/presentation/views/bodies/first_page/widgets/custom_searsh.dart';
+import 'package:nawy_app/features/_3_filter/bodies/filter_view_body.dart';
+import 'package:nawy_app/features/_3_filter/filter_view.dart';
 
 class FirstPagebody extends StatelessWidget {
   const FirstPagebody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [CustomHead()],
+    return const Column(
+      children: [
+        CustomHead(),
+        SizedBox(
+          height: 10,
+        ),
+        customSearsh(),
+      ],
     );
   }
 }

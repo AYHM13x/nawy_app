75f6ba9d864f48ccf688a3fa1ccd4cec6ecc4a39
mohamad darweshe <@gmail.com>
2024-06-01 10:18:24 +0300

    ubdate app

--- lib/features/_2_home/presentation/views/bodys/first_page/first_page/first_page_body.dart
+++ lib/features/_2_home/presentation/views/bodys/first_page/first_page/first_page_body.dart
@@ -5,7 +5,6 @@ import 'package:nawy_app/core/utlis/assets/font_styles.dart';
 import 'package:nawy_app/features/_2_home/presentation/views/bodys/first_page/first_page/widgets/Custom_List_View_Item.dart';
 import 'package:nawy_app/features/_2_home/presentation/views/bodys/first_page/first_page/widgets/custom_chose_bottom.dart';
 import 'package:nawy_app/features/_2_home/presentation/views/bodys/first_page/first_page/widgets/custom_head.dart';
-import 'package:nawy_app/features/_2_home/presentation/views/bodys/first_page/first_page/widgets/custom_places.dart';
 import '../../../widgets/main_view/custom_chose_bottom.dart';
 
 class FirstPagebody extends StatelessWidget {
@@ -14,83 +13,191 @@ class FirstPagebody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child: Column(
+        child: Column(children: [
+      const CustomHead(),
+      const SizedBox(
+        height: 10,
+      ),
+      const customSearsh(),
+      const SizedBox(
+        height: 10,
+      ),
+      const Customchosebotton(),
+      const Gap(16),
+      ListView(
+        shrinkWrap: true,
+        physics: NeverScrollableScrollPhysics(),
         children: [
-          const CustomHead(),
-          const SizedBox(
-            height: 10,
+          Padding(
+            padding: EdgeInsets.only(right: 23.sp),
+            child: Text("أفضل العقارات  :  ",
+                style:
+                    //FontStyles.textStyle14Reg
+                    TextStyle(fontSize: 14.sp, fontWeight: FontWeight.bold)),
           ),
-          const customSearsh(),
-          const SizedBox(
-            height: 10,
-          ),
-          Customchosebotton(),
-          Column(
-            children: [
-              Text(
-                "الأكثر شيوعا :",
-                style: FontStyles.textStyle16Bold,
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              CustomPlaces(
-                name: "assets/images/rowplaces.png",
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              CustomListViewItem(
-                name: "assets/images/dillon-kydd-XGvwt544g8k-unsplash 1.png",
-              ),
-            ],
+          CustomListViewItem(
+            name: "assets/images/pngs/dillon-kydd-XGvwt544g8k-unsplash 1.png",
           )
         ],
       ),
-    );
+    ]));
   }
 }
- 
-// القسم الاول 
-  // Column(
-  //           children: [
-  //             CustomListViewItem(),
-  //             SizedBox(
-  //               height: 20,
-  //             ),
-  //             Text(
-  //               "اهم الاماكن :",
-  //               style: FontStyles.textStyle16Bold,
-  //             ),
-  //             SizedBox(
-  //               height: 20,
-  //             ),
-      //         CustomPlaces(name: "assets/images/rowplaces.png",),
-  //             SizedBox(
-  //               height: 20,
-  //             ),
-  //             Text(
-  //               "اهم المؤثرين :",
-  //               style: FontStyles.textStyle16Bold,
-  //             ),
-  //             SizedBox(
-  //               height: 20,
-  //             ),
-  //             CustomInfluencers(),
-  //             SizedBox(
-  //               height: 20,
-  //             ),
-  //             Text(
-  //               "اهم العقارات القريبة :",
-  //               style: FontStyles.textStyle16Bold,
-  //             ),
-  //             SizedBox(
-  //               height: 20,
-  //             ),
-  //             CustomListViewItem(name: "assets/images/dillon-kydd-XGvwt544g8k-unsplash 1.png",),
-  //             SizedBox(
-  //               height: 20,
-  //             ),
-  //           ],
-  //         )
-        
+
+
+
+      //  الثالث
+          // Column(
+          //   children: [
+          //     CustomListViewItem(
+          //       name:
+          //           "assets/images/pngs/dillon-kydd-XGvwt544g8k-unsplash 1.png",
+          //     ),
+          //     SizedBox(
+          //       height: 20,
+          //     ),
+          //     Text(
+          //       "اهم الاماكن :",
+          //       style: FontStyles.textStyle16Bold,
+          //     ),
+          //     SizedBox(
+          //       height: 20,
+          //     ),
+          //     CustomPlaces(
+          //       name: "assets/images/pngs/rowplaces.png",
+          //     ),
+          //     SizedBox(
+          //       height: 20,
+          //     ),
+          //     Text(
+          //       "اهم المؤثرين :",
+          //       style: FontStyles.textStyle16Bold,
+          //     ),
+          //     SizedBox(
+          //       height: 20,
+          //     ),
+          //     CustomInfluencers(),
+          //     SizedBox(
+          //       height: 20,
+          //     ),
+          //     Text(
+          //       "اهم العقارات القريبة :",
+          //       style: FontStyles.textStyle16Bold,
+          //     ),
+          //     SizedBox(
+          //       height: 20,
+          //     ),
+          //     CustomListViewItem(
+          //       name: "assets/images/dillon-kydd-XGvwt544g8k-unsplash 1.png",
+          //     ),
+          //     SizedBox(
+          //       height: 20,
+          //     ),
+          //   ],
+          // )
+
+
+
+
+
+//الثاني
+// Column(
+//   children: [
+//     Text(
+//       "الأكثر شيوعا :",
+//       style: FontStyles.textStyle16Bold,
+//     ),
+//     SizedBox(
+//       height: 20,
+//     ),
+//     CustomPlaces(
+//       name: AppImages.rowPlace,
+//     ),
+//     SizedBox(
+//       height: 20,
+//     ),
+//     CustomListViewItem(
+//       name: AppImages.dillon,
+//     ),
+//   ],
+// )S
+
+
+
+// Column(
+//           children: [
+//             Text(
+//               "الأكثر شيوعا :",
+//               style: FontStyles.textStyle16Bold,
+//             ),
+//             const SizedBox(
+//               height: 20,
+//             ),
+//             const CustomPlaces(
+//               name: "assets/images/rowplaces.png",
+//             ),
+//             const SizedBox(
+//               height: 20,
+//             ),
+//             const CustomListViewItem(
+//               name: "assets/images/dillon-kydd-XGvwt544g8k-unsplash 1.png",
+//             ),
+//           ],
+//         )
+
+
+
+// class Description extends StatelessWidget {
+//   const Description({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Column(
+//       children: [
+//  const CustomListViewItem(
+//   name: "assets/images/dillon-kydd-XGvwt544g8k-unsplash 1.png",
+// ),
+//         const SizedBox(
+//           height: 20,
+//         ),
+//         Text(
+//           "اهم الاماكن :",
+//           style: FontStyles.textStyle16Bold,
+//         ),
+//         const SizedBox(
+//           height: 20,
+//         ),
+//         const CustomPlaces(
+//           name: "assets/images/rowplaces.png",
+//         ),
+//         const SizedBox(
+//           height: 20,
+//         ),
+//         Text(
+//           "اهم المؤثرين :",
+//           style: FontStyles.textStyle16Bold,
+//         ),
+//         const SizedBox(
+//           height: 20,
+//         ),
+//         const CustomInfluencers(),
+//         const SizedBox(
+//           height: 20,
+//         ),
+//         Text(
+//           "اهم العقارات القريبة :",
+//           style: FontStyles.textStyle16Bold,
+//         ),
+//         const SizedBox(
+//           height: 20,
+//         ),
+//         const CustomListViewItem(
+//           name: "assets/images/dillon-kydd-XGvwt544g8k-unsplash 1.png",
+//         ),
+//         const SizedBox(
+//           height: 20,
+//         ),
+//       ],
+//     );
+//   }
+// }

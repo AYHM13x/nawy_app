84a62aca9ae8ca49b8e9f6d3dd30662e56a4e829
mohamad darweshe <@gmail.com>
2024-06-04 13:58:10 +0300

    Finish aeshive

--- lib/features/_2_home/presentation/views/bodys/profile_view/Archives_View/widgets/Archives_View_Body.dart
+++ lib/features/_2_home/presentation/views/bodys/profile_view/Archives_View/widgets/Archives_View_Body.dart
@@ -5,6 +5,7 @@ import 'package:gap/gap.dart';
 import 'package:nawy_app/core/utlis/assets/app_colors.dart';
 import 'package:nawy_app/core/utlis/widgets/custom_elevated_botton.dart';
 import 'package:nawy_app/features/_2_home/presentation/views/bodys/profile_view/Archives_View/widgets/Custom_Chose_Every_One_And_Sale.dart';
+import 'package:nawy_app/features/_3_filter/bodies/fliter_view_after_search.dart';
 
 class ArchivesViewBody extends StatelessWidget {
   const ArchivesViewBody({super.key});
@@ -14,7 +15,11 @@ class ArchivesViewBody extends StatelessWidget {
     return Column(
       children: [
         Gap(10.h),
-        SingleChildScrollView(child: CustomChoseEveryOneAndSale())
+        SingleChildScrollView(
+          child: CustomChoseEveryOneAndSale(),
+        ),
+        Gap(5.h),
+        FliterViewAfterSearch()
       ],
     );
   }

652ec7dc45730f000766d3517169a95d3ccfe94c
mohamad darweshe <@gmail.com>
2024-06-05 10:48:50 +0300

    ubdate

--- lib/features/_2_home/presentation/views/bodys/profil_setting/Profil_Setting.body.dart
+++ lib/features/_2_home/presentation/views/bodys/profil_setting/Profil_Setting.body.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_chat_ui/flutter_chat_ui.dart';
+// import 'package:flutter_chat_ui/flutter_chat_ui.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:gap/gap.dart';
